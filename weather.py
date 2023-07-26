921d8f9600f19567a4bf6ec9721a54bf4ae524c9
kyledobash <kyledobash@yahoo.com>
2023-07-26 01:45:16 -0700

    menu button size change

--- weather.py
+++ weather.py
@@ -1,10 +1,9 @@
-import sys
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
 import requests
-from datetime import datetime
 from dotenv import load_dotenv
 import os
 
@@ -19,59 +18,86 @@ locations = {
     "Snowbasin": "101346_poi"         # Snowbasin Ski Resort, Utah
 }
 
-class SkiResortWeatherApp(App):
-    def build(self):
-        main_layout = BoxLayout(orientation="vertical")
+class MainMenuScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
 
-        # Create a label for the title
-        title_label = Label(text="Ski Resort Weather", halign="center")
-        main_layout.add_widget(title_label)
+        # Create buttons for each resort
+        for location, _ in locations.items():
+            button = Button(text=location)
+            button.bind(on_release=self.switch_to_resort_screen)
+            self.add_widget(button)
 
-        # Create a scroll view to hold the weather data
-        scroll_view = ScrollView()
-        main_layout.add_widget(scroll_view)
+    def switch_to_resort_screen(self, button):
+        app = App.get_running_app()
+        app.root.current = button.text
 
-        # Create a layout to hold the weather data labels
-        weather_layout = BoxLayout(orientation="vertical", spacing=10, padding=(10, 10, 10, 10))
-        scroll_view.add_widget(weather_layout)
+class ResortScreen(BoxLayout):
+    def __init__(self, location, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.location = location
 
-        self.result_labels = {}  # Dictionary to hold the weather data labels
-
-        for location, location_key in locations.items():
-            label = Label(text="Fetching weather data...", halign="left")
-            weather_layout.add_widget(label)
-            self.result_labels[location] = label
+        self.weather_label = Label(text="Fetching weather data...")
+        self.add_widget(self.weather_label)
 
         self.fetch_weather_data()
-        return main_layout
+
+        # Add a "Back to Menu" button with custom size
+        back_button = Button(text="Back to Menu", size_hint=(1, 0.1))  # Adjust the size here
+        back_button.bind(on_release=self.switch_to_main_menu)
+        self.add_widget(back_button)
 
     def fetch_weather_data(self):
-        for location, location_key in locations.items():
-            print(f"Fetching weather data for {location} (location key: {location_key})")
+        location_key = locations[self.location]
 
-            # Fetch current weather data from AccuWeather using location key
-            url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={ACCUWEATHER_API_KEY}"
+        # Fetch current weather data from AccuWeather using location key
+        current_params = {
+            "apikey": ACCUWEATHER_API_KEY,
+            "details": True
+        }
 
-            try:
-                current_response = requests.get(url)
-                current_data = current_response.json()
+        try:
+            current_response = requests.get(f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}", params=current_params)
+            current_data = current_response.json()
 
-                if current_data and isinstance(current_data, list):
-                    weather_data = current_data[0]
-                    current_temp = weather_data.get("Temperature", {}).get("Imperial", {}).get("Value")
-                    current_condition = weather_data.get("WeatherText")
+            if current_data and isinstance(current_data, list):
+                weather_data = current_data[0]
+                current_temp = weather_data.get("Temperature", {}).get("Imperial", {}).get("Value")
+                current_condition = weather_data.get("WeatherText")
 
-                    location_data = f"{location}\n"
-                    location_data += f"Current Temperature: {current_temp}°F\n"
-                    location_data += f"Current Conditions: {current_condition}\n"
-                    self.result_labels[location].text = location_data
-                else:
-                    self.result_labels[location].text = f"Weather data not available for {location}\n"
-            except requests.exceptions.RequestException as e:
-                self.result_labels[location].text = f"Failed to fetch weather data for {location}\n"
-                print(f"Error: {e}")
+                location_data = f"{self.location}\n"
+                location_data += f"Current Temperature: {current_temp}°F\n"
+                location_data += f"Current Conditions: {current_condition}\n"
+                self.weather_label.text = location_data
+            else:
+                self.weather_label.text = f"Weather data not available for {self.location}\n"
+        except requests.exceptions.RequestException as e:
+            self.weather_label.text = f"Failed to fetch weather data for {self.location}\n"
+            print(f"Error: {e}")
 
-# Run the application
-if __name__ == "__main__":
+    def switch_to_main_menu(self, button):
+        app = App.get_running_app()
+        app.root.current = 'Main Menu'
+
+class SkiResortWeatherApp(App):
+    def build(self):
+        self.screen_manager = ScreenManager()
+
+        # Add the main menu screen
+        main_menu_screen = Screen(name='Main Menu')
+        main_menu_screen.add_widget(MainMenuScreen())
+        self.screen_manager.add_widget(main_menu_screen)
+
+        # Add resort-specific screens
+        for location, _ in locations.items():
+            resort_screen = Screen(name=location)
+            resort_screen.add_widget(ResortScreen(location))
+            self.screen_manager.add_widget(resort_screen)
+
+        return self.screen_manager
+
+if __name__ == '__main__':
     app = SkiResortWeatherApp()
     app.run()
