d2db0499793e85602b67f2909623d5e6e48672ab
kyledobash <kyledobash@yahoo.com>
2023-07-29 14:51:34 -0700

    fixed environment

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dotenv',
+    version='0.0.5',
+    author='motdotla',
+    author_email='motdotla@gmail.com',
+    url='https://github.com/motdotla/dotenv',
+    description='Loads environment variables from .env for nodejs projects.',
+    packages=['dotenv'],
+)
